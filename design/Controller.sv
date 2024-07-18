3a92e3fa1c57ce4362357e42dd06202ae01d563e
Lucas Emanuel <131834616+llucasEmanuel@users.noreply.github.com>
2024-07-18 16:36:54 -0300

    adição do opcode de instruções lógico-aritméticas imediatas

--- design/Controller.sv
+++ design/Controller.sv
@@ -19,19 +19,21 @@ module Controller (
     output logic Branch  //0: branch is not taken; 1: branch is taken
 );
 
-  logic [6:0] R_TYPE, LW, SW, BR;
+  logic [6:0] R_TYPE, I_TYPE_IMM, LW, SW, BR;
 
+  // atribuição dos opcodes
   assign R_TYPE = 7'b0110011;  //add,and
+  assign I_TYPE_IMM = 7'b0010011; // aritmeticas e logicas imediatas (addi, ...)
   assign LW = 7'b0000011;  //lw
   assign SW = 7'b0100011;  //sw
   assign BR = 7'b1100011;  //beq
 
-  assign ALUSrc = (Opcode == LW || Opcode == SW);
+  assign ALUSrc = (Opcode == LW || Opcode == SW || Opcode == I_TYPE_IMM);
   assign MemtoReg = (Opcode == LW);
-  assign RegWrite = (Opcode == R_TYPE || Opcode == LW);
+  assign RegWrite = (Opcode == R_TYPE || Opcode == LW || Opcode == I_TYPE_IMM);
   assign MemRead = (Opcode == LW);
   assign MemWrite = (Opcode == SW);
   assign ALUOp[0] = (Opcode == BR);
-  assign ALUOp[1] = (Opcode == R_TYPE);
+  assign ALUOp[1] = (Opcode == R_TYPE || Opcode == I_TYPE_IMM);
   assign Branch = (Opcode == BR);
 endmodule
