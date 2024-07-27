c8f9ffd516a602b339d57041d59094fd2386d7e5
Lucas Emanuel <131834616+llucasEmanuel@users.noreply.github.com>
2024-07-27 18:24:59 -0300

    adição de

--- design/Controller.sv
+++ design/Controller.sv
@@ -16,7 +16,9 @@ module Controller (
     output logic MemRead,  //Data memory contents designated by the address input are put on the Read data output
     output logic MemWrite, //Data memory contents designated by the address input are replaced by the value on the Write data input.
     output logic [1:0] ALUOp,  //00: LW/SW; 01:Branch; 10: Rtype
-    output logic Branch  //0: branch is not taken; 1: branch is taken
+    output logic Branch,  //0: branch is not taken; 1: branch is taken
+    output logic JalrSel,
+    output logic [1:0] RWSel,
 );
 
   logic [6:0] R_TYPE, I_TYPE_IMM, LW, SW, BR, JAL, JALR, HALT;
@@ -24,19 +26,24 @@ module Controller (
   // atribuição dos opcodes
   assign R_TYPE = 7'b0110011;  //add,and
   assign I_TYPE_IMM = 7'b0010011; // aritmeticas e logicas imediatas (addi, ...)
-  assign LW = 7'b0000011;  //lw
-  assign SW = 7'b0100011;  //sw
-  assign BR = 7'b1100011;  //beq, bne
+  assign LW = 7'b0000011;  //lw, lh, lb, lbu
+  assign SW = 7'b0100011;  //sw, sh, sb
+  assign BR = 7'b1100011;  //beq, bne, blt
   assign JAL = 7'b1101111; // jal (j-type)
   assign JALR = 7'b1100111; // jalr (i-type)
   assign HALT = 7'b1111111; // halt (halt-type -> não existe)
-
-  assign ALUSrc = (Opcode == LW || Opcode == SW || Opcode == I_TYPE_IMM || Opcode == JAL || Opcode == JALR);
+  
+                                                                        // não vai usar a ALU pra a soma (vai usar um adder só)
+  assign ALUSrc = (Opcode == LW || Opcode == SW || Opcode == I_TYPE_IMM /*|| Opcode == JAL */|| Opcode == JALR);
   assign MemtoReg = (Opcode == LW);
   assign RegWrite = (Opcode == R_TYPE || Opcode == LW || Opcode == I_TYPE_IMM || Opcode == JAL || Opcode == JALR);
   assign MemRead = (Opcode == LW);
-  assign MemWrite = (Opcode == SW);
-  assign ALUOp[0] = (Opcode == BR || Opcode == JAL || Opcode == JALR);
-  assign ALUOp[1] = (Opcode == R_TYPE || Opcode == I_TYPE_IMM || Opcode == JAL || Opcode == JALR);
-  assign Branch = (Opcode == BR || Opcode == JAL || Opcode == JALR);
+  assign MemWrite = (Opcode == SW); 
+  assign ALUOp[0] = (Opcode == BR || Opcode == JAL/* || Opcode == JALR*/);
+  assign ALUOp[1] = (Opcode == R_TYPE || Opcode == I_TYPE_IMM || Opcode == JAL /*|| Opcode == JALR*/);
+  assign Branch = (Opcode == BR || Opcode == JAL /*|| Opcode == JALR*/);
+  /* assign JalrSel = */
+  assign RWSel[0] = (Opcode == JAL) // guarda o valor do PC+4 no reg 
+  assign RWSel[1] = 0; // dps mudar isso 
+
 endmodule
